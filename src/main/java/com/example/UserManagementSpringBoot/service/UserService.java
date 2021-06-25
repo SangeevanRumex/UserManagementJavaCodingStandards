19b59dc8a8de4bca00e81f21717e79f6f8369aa0
SangeevanRumex <sangeevan@rumex.lk>
2021-06-25 11:57:38 +0530

    Add copy right statement

--- src/main/java/com/example/UserManagementSpringBoot/service/UserService.java
+++ src/main/java/com/example/UserManagementSpringBoot/service/UserService.java
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * (C) Copyright 2010-2021 RUMEX (PRIVATE) LIMITED                            *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ * These materials are unpublished, proprietary, confidential source code of  *
+ * RUMEX (PRIVATE) LIMITED and constitute a TRADE SECRET                      *
+ * of RUMEX (PRIVATE) LIMITED                                                 *
+ *                                                                            *
+ * RUMEX (PRIVATE) LIMITED retains all title to and intellectual              *
+ * property rights in these materials.                                        *
+ ******************************************************************************/
+
 package com.example.UserManagementSpringBoot.service;
 
 import com.example.UserManagementSpringBoot.model.dto.UserDto;
